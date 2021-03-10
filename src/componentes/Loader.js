45fc109478402bc6fe9421a13be627956703f551
Joaquín Villanueva <villanuevajoaquin94@gmail.com>
2021-03-10 11:59:54 -0300

    Alerta agregada en loader indicando la deshabilitación momentánea del conversor de monedas

--- src/componentes/Loader.js
+++ src/componentes/Loader.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Gif from "../imgs/loader.gif";
 
 const Loader = () => {
+  useEffect(() => {
+    window.alert(
+      "Deshabilitada la posibilidad de calcular el tipo de cambio hasta que se arregle el error de CORS en la API de Free Currency Converter."
+    );
+  }, []);
+
   return (
     <div className="loader">
       <img src={Gif} alt="loader" />
